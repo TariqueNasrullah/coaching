0eb99180eb1d32939d6042e664517dc74afc2acd
TariqueNasrullah <nasrullahtarique@gmail.com>
2020-07-25 16:26:49 +0600

    final

---
+++ static/static/eventManager/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from eventManager.models import event
+
+admin.site.register(event)
---
+++ static/static/teachers/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from teachers.models import teacher
+
+admin.site.register(teacher)
